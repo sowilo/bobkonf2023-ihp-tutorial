94b78a12f1d74765856c109cf19b54ffd6cba1f5
Bianca Lutz <bianca.lutz@active-group.de>
2023-03-15 19:50:48 +0100

    Setup mail server

--- bobig/Config/Config.hs
+++ bobig/Config/Config.hs
@@ -4,9 +4,16 @@ import IHP.Prelude
 import IHP.Environment
 import IHP.FrameworkConfig
 import Web.View.CustomCSSFramework
+import IHP.Mail
 
 config :: ConfigBuilder
 config = do
     -- See https://ihp.digitallyinduced.com/Guide/config.html
     -- for what you can do here
     option customBootstrap
+    option $
+        SMTP { host = "127.0.1.1"
+             , port = 1025
+             , credentials = Nothing
+             , encryption = Unencrypted
+             }
--- bobig/default.nix
+++ bobig/default.nix
@@ -15,6 +15,7 @@ let
         ];
         otherDeps = p: with p; [
             # Native dependencies, e.g. imagemagick
+            mailhog # dummy SMTP server
         ];
         projectPath = ./.;
         withHoogle = true;
