15f287b54ce7bd7d069d737d614a1fa87ccf8c73
Bianca Lutz <bianca.lutz@active-group.de>
2023-03-15 13:33:06 +0100

    Add contacts table

--- bobig/Application/Migration/1678883536.sql
+++ bobig/Application/Migration/1678883536.sql
@@ -1,8 +1,7 @@
--- Your database schema. Use the Schema Designer at http://localhost:8001/ to add some tables.
 CREATE TABLE contacts (
     id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
     name TEXT NOT NULL,
     email TEXT NOT NULL,
-    phone TEXT DEFAULT NULL,
+    phone TEXT DEFAULT null,
     date_of_birth DATE NOT NULL
 );
