4b230095893377bb73b4e5ad0baba00162b44a4d
Bianca Lutz <bianca.lutz@active-group.de>
2023-03-16 10:39:32 +0100

    Activate send mail job worker

--- bobig/Main.hs
+++ bobig/Main.hs
@@ -8,6 +8,7 @@ import IHP.FrameworkConfig
 import IHP.Job.Types
 import Web.FrontController
 import Web.Types
+import Web.Worker
 
 instance FrontController RootApplication where
     controllers = [
@@ -15,7 +16,7 @@ instance FrontController RootApplication where
         ]
 
 instance Worker RootApplication where
-    workers _ = []
+    workers _ = workers WebApplication
 
 main :: IO ()
 main = IHP.Server.run config
